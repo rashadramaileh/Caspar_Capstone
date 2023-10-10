637748b755326bb48ef4503a8388abf57261267c
Alyshay98 <97034708+Alyshay98@users.noreply.github.com>
2023-10-10 11:34:22 -0600

    Added Index for Students Added Edit and Delete Pages to the Students file added a javascript file for Student Wishlists for datatable

--- Pages/Students/Edit.cshtml.cs
+++ Pages/Students/Edit.cshtml.cs
@@ -1,12 +1,12 @@
-using DataAccess;
-using CASPAR.Infrastructure.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CASPAR.Pages.Students
 {
-    public class IndexModel : PageModel
+    public class EditModel : PageModel
     {
-        
+        public void OnGet()
+        {
+        }
     }
 }
