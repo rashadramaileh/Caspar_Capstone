d4ffd229327e381e4b82b3da6a86bc2db2b5c3ac
LightEngineARC <i.am.spiritman@gmail.com>
2023-10-24 18:51:55 -0600

    add major index, upsert, delete, and js for datatables

---
+++ Pages/Majors/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+using CASPAR.Infrastructure.Models;
+using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CASPAR.Pages.Majors
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly UnitOfWork _unitOfWork;
+        [BindProperty]
+        public Major objMajor { get; set; }
+        public DeleteModel(UnitOfWork unit, IWebHostEnvironment web)
+        {
+            _unitOfWork = unit;
+            objMajor = new Major();
+        }
+        public IActionResult OnGet(int? id)
+        {
+
+            objMajor = _unitOfWork.Major.GetById(id);
+            if (objMajor == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+        public IActionResult OnPost(int? id)
+        {
+            var objProduct = _unitOfWork.Major.GetById(id);
+            if (objProduct == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Major.Delete(objProduct);
+            TempData["success"] = "Major Deleted Successfully";
+            _unitOfWork.Commit();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Majors/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CASPAR.Pages.Majors
+{
+    public class IndexModel : PageModel
+    {
+        // Blank - DataTables handles this with JavaScript
+    }
+}
