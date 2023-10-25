9f8a8fd78fb9dfa6982d4d7fbfc813f89cdbe05d
LightEngineARC <i.am.spiritman@gmail.com>
2023-10-24 18:51:55 -0600

    add major index, upsert, delete, and js for datatables

---
+++ Controllers/MajorController.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CASPAR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MajorController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public MajorController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.Major.GetAll() });
+        }
+    }
+}
