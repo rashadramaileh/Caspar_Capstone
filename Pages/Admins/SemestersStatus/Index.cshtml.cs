b5f3241811a794fddf0e8e73809ff8c53c7bbf19
Austin Wilson <austinwilson@mail.weber.edu>
2023-10-27 10:02:15 -0600

    Adding Semester Status and Type

---
+++ Pages/Admins/SemestersStatus/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CASPAR.Pages.Admins.SemestersStatus
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
