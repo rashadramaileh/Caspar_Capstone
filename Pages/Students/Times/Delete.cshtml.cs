68fd9d6ebeef2cec2edfbb9d02fbb9a1bac99138
Austin Wilson <austinwilson@mail.weber.edu>
2023-10-28 11:35:59 -0600

    More Stuff

--- Pages/Students/Times/Delete.cshtml.cs
+++ Pages/Students/Times/Delete.cshtml.cs
@@ -1,3 +1,5 @@
+using CASPAR.Infrastructure.Models;
+using DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,47 @@ namespace CASPAR.Pages.Students.Times
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly UnitOfWork _unitOfWork;
+        [BindProperty]
+        public StudentTime objTimeList { get; set; }
+        public DeleteModel(UnitOfWork unitOfWOrk)
         {
+            _unitOfWork = unitOfWOrk;
+            objTimeList = new StudentTime();
+        }
+        public IActionResult OnGet(int? id)
+        {
+            if (id != 0)
+            {
+                objTimeList = _unitOfWork.StudentTime.GetById(id);
+            }
+
+            if (objTimeList == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+
+        }
+
+        public IActionResult OnPost()
+        {
+            string redirect = "./Index?id=" + objTimeList.StudentWishlistModalityId;
+
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Data Error unable to connect to DB";
+                return Page();
+            }
+            else
+            {
+                _unitOfWork.StudentTime.Delete(objTimeList);
+                TempData["success"] = "Modality Deleted Successfully";
+            }
+
+            _unitOfWork.Commit();
+            return Redirect(redirect);
         }
     }
 }
