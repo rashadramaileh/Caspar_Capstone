9e0e23c9bed9e10976f1fb46f4b8622d8375b034
Austin Wilson <austinwilson@mail.weber.edu>
2023-10-28 10:30:52 -0600

    More Stuff

---
+++ Pages/Students/Times/Upsert.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CASPAR.Pages.Students.Times
+{
+    public class UpsertModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
