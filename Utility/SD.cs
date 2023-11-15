a541d1b9c858d0779cd2a1c30635dddc5ba6682d
Rashad Ramaileh <rashadramaileh@gmail.com>
2023-11-14 18:36:46 -0700

    added utility folder

---
+++ Utility/SD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class SD
+    {
+        public const string AdminRole = "Admin";
+        public const string InstructorRole = "Instructor";
+        public const string StudentRole = "Student";
+    }
+}
+
