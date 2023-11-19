eae779351d5f20534133d5b5b58f3ec79c9945fe
Alyshay98 <97034708+Alyshay98@users.noreply.github.com>
2023-11-19 10:17:22 -0700

    Added a data table to the release time page

---
+++ wwwroot/js/InstructorReleaseTime.js
@@ -0,0 +1,36 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_ReleaseTime').DataTable({
+        "ajax": {
+            "url": "/api/instructorreleasetime",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            // Should not be capital
+            { "data": "instructor.name", "width": "70%" },
+            { "data": "instructorrelease.releasetimeamount", "width": "70%" },
+            { "data": "instructorrelease.releasetimeamount", "width": "70%" },
+            { "data": "instructorrelease.releasetimeamount", "width": "70%" },
+            { "data": "instructorrelease.releasenotes", "width": "70%" },
+            {
+                "data": "instructorReleaseId",
+                "render": function (data) {
+                    return `<div class="text-center"> 
+                                <a href="/Admin/InstructorReleaseTimes/InstructorReleaseTime?id=${data}" class="btn btn-primary text-white" style="cursor:pointer; width: 100px;">
+                                    <i class="far fa-edit"></i> View </a>
+                        </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found."
+        },
+        "width": "100%",
+    });
+}
