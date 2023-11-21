459497f540fb3b6216a3de510bc469486221c2cd
LightEngineARC <i.am.spiritman@gmail.com>
2023-11-20 21:47:15 -0700

    minor change

--- Pages/Admins/InstructorReleaseTimes/InstructorReleaseTime.cshtml.cs
+++ Pages/Admins/InstructorReleaseTimes/InstructorReleaseTime.cshtml.cs
@@ -1,32 +1,42 @@
 using CASPAR.Infrastructure.Models;
 using DataAccess;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CASPAR.Pages.Admins.InstructorReleaseTimes
 {
     public class InstructorReleaseTimeModel : PageModel
     {
         private readonly UnitOfWork _unitOfWork;
-        public InstructorRelease objReleaseList { get; set; }
+        public InstructorRelease objRelease { get; set; }
+        public IEnumerable<InstructorRelease> objReleaseList { get; set; }
         public int UserId { get; set; } = 1;
 
-        [BindProperty]
         public int SemesterId { get; set; }
         public IEnumerable<SelectListItem> SemesterList { get; set; }
+        public Semester objSemester { get; set; }
 
         public InstructorReleaseTimeModel(UnitOfWork unitOfWork)
         {
+            objRelease = new InstructorRelease();
             _unitOfWork = unitOfWork;
             SemesterList = new List<SelectListItem>();
-            objReleaseList = new InstructorRelease();
+            objReleaseList = new List<InstructorRelease>();
+            objSemester = new Semester();
+
         }
 
-        public void OnGet()
+    public void OnGet()
         {
-            GetReleaseData(UserId, null);
+            //GetReleaseData(UserId, null);
+            SemesterList = _unitOfWork.Semester.GetAll()
+                .Select(x => new SelectListItem
+                {
+                    Text = x.SemesterName,
+                    Value = x.SemesterId.ToString(),
+                });
+            objReleaseList = _unitOfWork.InstructorRelease.GetAll();
+
         }
 
         public void OnPost() 
@@ -45,28 +55,28 @@ namespace CASPAR.Pages.Admins.InstructorReleaseTimes
 
             if (semesterId != null && semesterId != 0)
             {
-                objReleaseList = _unitOfWork.InstructorRelease.Get(x => x.UserId == instructorId && x.SemesterId == semesterId);
+                objRelease = _unitOfWork.InstructorRelease.Get(x => x.UserId == instructorId && x.SemesterId == semesterId);
             }
             else
             {
-                objReleaseList = _unitOfWork.InstructorRelease.Get(x => x.UserId == instructorId);
+                objRelease = _unitOfWork.InstructorRelease.Get(x => x.UserId == instructorId);
             }
 
-            if (objReleaseList == null)
+            if (objRelease == null)
             {
-                objReleaseList = new InstructorRelease
+                objRelease = new InstructorRelease
                 {
                     UserId = instructorId,
                     SemesterId = semesterId ?? 1,
                 };
-                _unitOfWork.InstructorRelease.Add(objReleaseList);
+                _unitOfWork.InstructorRelease.Add(objRelease);
             }
 
-            var instrucorReleaseTimeDetails = _unitOfWork.InstructorRelease.GetAll(x => x.InstructorReleaseId == objReleaseList.InstructorReleaseId, null, null);
+            var instrucorReleaseTimeDetails = _unitOfWork.InstructorRelease.GetAll(x => x.InstructorReleaseId == objRelease.InstructorReleaseId, null, null);
 
             foreach (var item in instrucorReleaseTimeDetails)
             {
-                InstructorRelease toAdd = objReleaseList;
+                InstructorRelease toAdd = objRelease;
                 _unitOfWork.InstructorRelease.Add(toAdd);
             }
 
