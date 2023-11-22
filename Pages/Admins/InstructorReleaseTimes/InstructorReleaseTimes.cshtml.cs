134983759491f11fe516168e0428001f4db256a1
buxxmoney <87129838+buxxmoney@users.noreply.github.com>
2023-11-21 22:58:37 -0700

    Added Pages

---
+++ Pages/Admins/InstructorReleaseTimes/InstructorReleaseTimes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CASPAR.Pages.Admins.InstructorReleaseTimes
+{
+    public class InstructorReleaseTimesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
