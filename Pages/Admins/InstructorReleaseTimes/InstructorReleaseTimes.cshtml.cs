8874dbbba5bb0cb2bb436a94e7a7a9ac1cf4ffa4
buxxmoney <87129838+buxxmoney@users.noreply.github.com>
2023-11-22 13:34:27 -0700

    Try

--- Pages/Admins/InstructorReleaseTimes/InstructorReleaseTimes.cshtml.cs
+++ Pages/Admins/InstructorReleaseTimes/InstructorReleaseTimes.cshtml.cs
@@ -2,13 +2,18 @@ using CASPAR.Infrastructure.Models;
 using DataAccess;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity; // Add this for UserManager and ApplicationUser
+using System.Security.Claims;
+using System.Linq;
 
 namespace CASPAR.Pages.Admins.InstructorReleaseTimes
 {
     public class InstructorReleaseTimeModel : PageModel
     {
         private readonly UnitOfWork _unitOfWork;
-        public InstructorRelease objRelease { get; set; }
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IEnumerable<Instructor> objInstructorList { get; set; }
         public IEnumerable<InstructorRelease> objReleaseList { get; set; }
         public int UserId { get; set; } = 1;
 
@@ -16,42 +21,43 @@ namespace CASPAR.Pages.Admins.InstructorReleaseTimes
         public IEnumerable<SelectListItem> SemesterList { get; set; }
         public Semester objSemester { get; set; }
 
-        public InstructorReleaseTimeModel(UnitOfWork unitOfWork)
+        public InstructorReleaseTimeModel(UnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
         {
-            objRelease = new InstructorRelease();
             _unitOfWork = unitOfWork;
+            _userManager = userManager;
             SemesterList = new List<SelectListItem>();
-            objReleaseList = new List<InstructorRelease>();
             objSemester = new Semester();
-
         }
 
         public void OnGet()
         {
-            GetReleaseData(null);
-            //SemesterList = _unitOfWork.Semester.GetAll()
-            //    .Select(x => new SelectListItem
-            //    {
-            //        Text = x.SemesterName,
-            //        Value = x.SemesterId.ToString(),
-            //    });
-            //objReleaseList = _unitOfWork.InstructorRelease.GetAll();
-
+            GetInstructorsAndReleaseData(null);
         }
 
         public void OnPost()
         {
-            GetReleaseData(SemesterId);
+            GetInstructorsAndReleaseData(SemesterId);
         }
 
-        private void GetReleaseData(int? semesterId)
+        private void GetInstructorsAndReleaseData(int? semesterId)
         {
+            // Get the current user's id
+            var currentUserId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            // Fetch the user's roles
+            var userRoles = HttpContext.User.FindAll(ClaimTypes.Role).Select(r => r.Value);
+
+            // Fetch instructors for the current user and role
+            objInstructorList = _unitOfWork.Instructor.GetAll(instructor =>
+                instructor.Id == currentUserId && userRoles.Contains("YourRole"));
+
+            // Fetch semesters for the dropdown
             SemesterList = _unitOfWork.Semester.GetAll()
                 .Select(x => new SelectListItem
                 {
                     Text = x.SemesterName,
                     Value = x.SemesterId.ToString(),
-                });     
+                });
 
             if (semesterId != null && semesterId != 0)
             {
@@ -60,26 +66,7 @@ namespace CASPAR.Pages.Admins.InstructorReleaseTimes
             else
             {
                 objReleaseList = _unitOfWork.InstructorRelease.GetAll(c => c.SemesterId == _unitOfWork.Semester.GetById(1).SemesterId);
-
             }
-
-            //if (objRelease == null)
-            //{
-            //    objRelease = new InstructorRelease
-            //    {
-            //        UserId = instructorId,
-            //        SemesterId = semesterId ?? 1,
-            //    };
-            //    _unitOfWork.InstructorRelease.Add(objRelease);
-            //}
-
-            //var instrucorReleaseTimeDetails = _unitOfWork.InstructorRelease.GetAll(x => x.InstructorReleaseId == objRelease.InstructorReleaseId, null, null);
-
-            //foreach (var item in instrucorReleaseTimeDetails)
-            //{
-            //    InstructorRelease toAdd = objRelease;
-            //    _unitOfWork.InstructorRelease.Add(toAdd);
-            //}
 
             TempData["success"] = "Semester Filtered Successfully";
         }
