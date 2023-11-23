4ed22409fd89870cad4e73f38e469254afda6917
Austin Wilson <austinwilson@mail.weber.edu>
2023-11-22 21:14:41 -0700

    Visual Changes and Course Sort

--- Pages/Students/StudentWishlistHome.cshtml.cs
+++ Pages/Students/StudentWishlistHome.cshtml.cs
@@ -58,7 +58,7 @@ namespace CASPAR.Pages.Students
 
         private void GetStudentWishlistData(string applicationUser, int? semesterId)
         {
-            SemesterList = _unitOfWork.Semester.GetAll()
+            SemesterList = _unitOfWork.Semester.GetAll().OrderBy(x => x.EndDate)
                 .Select(x => new SelectListItem
                 {
                     Text = x.SemesterName,
