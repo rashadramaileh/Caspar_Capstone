ae8b42786d0631c1f7acbc8555c13c9007b9bd6b
LightEngineARC <i.am.spiritman@gmail.com>
2023-12-01 23:42:01 -0700

    manually making the sections for the json data

--- Controllers/ProgramCoordinatorController.cs
+++ Controllers/ProgramCoordinatorController.cs
@@ -1,5 +1,7 @@
-﻿using DataAccess;
+﻿using CASPAR.Infrastructure.Models;
+using DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 
 namespace CASPAR.Controllers
@@ -18,11 +20,48 @@ namespace CASPAR.Controllers
         [HttpGet]
         public IActionResult OnGet(int? id)
         {
+            IEnumerable<Section> sections = _unitOfWork.Section.GetAll(null, null,
+"Semester,Classroom,Campus,Modality,ApplicationUser,MeetingTime,DayBlock,PartOfTerm,PayModel,WhoPays,SectionStatus")
+                .Where(x => x.SemesterId == id);
+            Semester semester = _unitOfWork.Semester.GetById(id);
+            IEnumerable<Campus> campuses = _unitOfWork.Campus.GetAll();
+            IEnumerable<Building> buildings = _unitOfWork.Building.GetAll();
+            IEnumerable<Course> courses = _unitOfWork.Course.GetAll();
+            IEnumerable<Modality> modalities = _unitOfWork.Modality.GetAll();
+            IEnumerable<ApplicationUser> applicationUsers = _unitOfWork.ApplicationUser.GetAll(); //filter for instructors
+            IEnumerable<MeetingTime> meetingTimes = _unitOfWork.MeetingTime.GetAll();
+            IEnumerable<DayBlock> dayBlocks = _unitOfWork.DayBlock.GetAll();
+            IEnumerable<Classroom> classrooms = _unitOfWork.Classroom.GetAll();
+            IEnumerable<PartOfTerm> partOfTerms = _unitOfWork.PartOfTerm.GetAll();
+            IEnumerable<PayModel> payModels = _unitOfWork.PayModel.GetAll();
+            IEnumerable<WhoPays> whoPayss = _unitOfWork.WhoPays.GetAll();
+            IEnumerable<SectionStatus> sectionStatuses = _unitOfWork.SectionStatus.GetAll();
+
+            //Loop to add all of these items to the Section
+            foreach (Section s in sections)
+            {
+                s.Semester = semester;
+                s.Classroom = classrooms.FirstOrDefault(i => i.ClassroomId == s.ClassroomId);
+                s.Classroom.Building = buildings.FirstOrDefault(b => b.BuildingId == s.Classroom.BuildingId);
+                s.Classroom.Building.Campus = campuses.FirstOrDefault(c => c.CampusId == s.Classroom.Building.CampusId);
+                s.Course = courses.FirstOrDefault(c => c.CourseId == s.CourseId);
+                s.Modality = modalities.FirstOrDefault(m => m.ModalityId == s.ModalityId);
+                s.ApplicationUser = applicationUsers.FirstOrDefault(a => a.Id == s.ApplicationUserId);
+                s.MeetingTime = meetingTimes.FirstOrDefault(m => m.meetingTimeId == s.MeetingTimeId);
+                s.DayBlock = dayBlocks.FirstOrDefault(d => d.DaysBlockId == s.DayBlockId);
+                s.PartOfTerm = partOfTerms.FirstOrDefault(p => p.PartOfTermID == s.PartOfTermId);
+                s.PayModel = payModels.FirstOrDefault(p => p.PayModelId == s.PayModelId);
+                s.WhoPays = whoPayss.FirstOrDefault(w => w.WhoPaysId == s.WhoPaysId);
+                s.SectionStatus = sectionStatuses.FirstOrDefault(t=> t.SectionStatusId == s.SectionStatusId);
+
+            }
+
+
             return Json(new
             {
-                data = _unitOfWork.Section.GetAll(null, null, "Campus,Course,Modality,ApplicationUser,MeetingTime,DayBlock,Classroom,PartOfTerm,PayModel,WhoPays,SectionStatus,Semester")
-                .Where(x => x.SemesterId == id)
-            });
+
+                data = sections
+            }); ;
         }
     }
 }
--- Infrastructure/Models/Section.cs
+++ Infrastructure/Models/Section.cs
@@ -5,6 +5,7 @@ namespace CASPAR.Infrastructure.Models
 {
     public class Section
     {
+        //Local Properties
         [Key, Display(Name = "Section")]
         public int SectionId { get; set; }
 
@@ -23,71 +24,100 @@ namespace CASPAR.Infrastructure.Models
         [StringLength(200), Display(Name = "Notes")]
         public string? Notes { get; set; }
 
+
+
+        //Relations
+
         [Range(0, 65535), Display(Name = "Classroom")]
         public int? ClassroomId { get; set; }
-
-        [Range(0, 65535), Required, Display(Name = "Course")]
-        public int CourseId { get; set; }
-
-        [Range(0, 65535), Display(Name = "Instructor")]
-        public string? ApplicationUserId { get; set; }
-
-        [Range(0, 65535), Display(Name = "Section Status")]
-        public int? SectionStatusId { get; set; }
-
-        [Range(0, 65535), Display(Name = "Who Pays")]
-        public int? WhoPaysId { get; set; }
-
-        [Range(0, 65535), Display(Name = "Pay Model")]
-        public int? PayModelId { get; set; }
-
-        [Range(0, 65535), Display(Name = "Part of Term")]
-        public int? PartofTermId { get; set; }
-
-        [Range(0, 255), Display(Name = "Modality")]
-        public int? ModalityId { get; set; }
-
-        [Range(0, 255), Display(Name = "Meeting Time")]
-        public int? MeetingTimeId { get; set; }
-
-        [Range(0, 255), Display(Name = "Day Block")]
-        public int? DayBlockId { get; set; }
-
-        [Required, Range(0, 65535), Display(Name = "Semester")]
-        public int SemesterId { get; set; }
-        
         [ForeignKey("ClassroomId")]
         public Classroom? Classroom { get; set; }
 
+
+        [Range(0, 65535), Required, Display(Name = "Course")]
+        public int CourseId { get; set; }
         [ForeignKey("CourseId")]
         public Course? Course { get; set; }
 
+
+        [Range(0, 65535), Display(Name = "Instructor")]
+        public string? ApplicationUserId { get; set; }
         [ForeignKey("ApplicationUserId")]
         public ApplicationUser? ApplicationUser { get; set; }
 
+
+        [Range(0, 65535), Display(Name = "Section Status")]
+        public int? SectionStatusId { get; set; }
         [ForeignKey("SectionStatusId")]
         public SectionStatus? SectionStatus { get; set; }
 
+
+        [Range(0, 65535), Display(Name = "Who Pays")]
+        public int? WhoPaysId { get; set; }
         [ForeignKey("WhoPaysId")]
         public WhoPays? WhoPays { get; set; }
 
+
+        [Range(0, 65535), Display(Name = "Pay Model")]
+        public int? PayModelId { get; set; }
         [ForeignKey("PayModelId")]
         public PayModel? PayModel { get; set; }
 
-        //[ForeignKey("PartOfTermId")]
-       //public PartOfTerm? PartOfTerm { get; set; }
 
+        [Range(0, 255), Display(Name = "Part of Term")]
+        public int? PartofTermId { get; set; }
+        [ForeignKey("PartOfTermId")]
+        public PartOfTerm? PartOfTerm { get; set; }
+
+
+        [Range(0, 255), Display(Name = "Modality")]
+        public int? ModalityId { get; set; }
         [ForeignKey("ModalityId")]
         public Modality? Modality { get; set; }
 
+
+        [Range(0, 255), Display(Name = "Meeting Time")]
+        public int? MeetingTimeId { get; set; }
         [ForeignKey("MeetingTimeId")]
         public MeetingTime? MeetingTime { get; set; }
 
+
+        [Range(0, 255), Display(Name = "Day Block")]
+        public int? DayBlockId { get; set; }
         [ForeignKey("DayBlockId")]
         public DayBlock? DayBlock { get; set; }
 
+
+        [Required, Range(0, 65535), Display(Name = "Semester")]
+        public int SemesterId { get; set; }
         [ForeignKey("SemesterId")]
         public Semester? Semester { get; set; }
+
+
+        //public int CampusId { get; set; }
+        [ForeignKey("CampusId")]
+        public Campus? Campus { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        
+
+        
+
+        
         
     }
 }
