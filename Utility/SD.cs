078a044042993a99d8e1d047acc7ce8e1ad52bee
Rashad Ramaileh <63267886+rashadramaileh@users.noreply.github.com>
2023-12-04 18:51:26 -0700

    added secretary and program coordinator roles

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -11,5 +11,7 @@ namespace Utility
         public const string AdminRole = "Admin";
         public const string InstructorRole = "Instructor";
         public const string StudentRole = "Student";
-    }
+        public const string SecretaryRole = "Secretary";
+        public const string ProgramCoordinatorRole = "Program Coordinator";
+    } 
 }
